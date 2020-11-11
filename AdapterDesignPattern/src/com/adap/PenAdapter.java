1ff51747cf6a73108984c643cc2c1d9328f0af6a
Satya <72313952+kspsatyaprasad@users.noreply.github.com>
2020-11-11 15:52:55 +0530

    Added Code For Adapter Design Pattern In JAVA

---
+++ AdapterDesignPattern/src/com/adap/PenAdapter.java
@@ -0,0 +1,13 @@
+package com.adap;
+
+import com.pilotpen.PilotPen;
+
+public class PenAdapter implements Pen {
+    
+	PilotPen pp = new PilotPen();
+	@Override
+	public void write(String str) {
+		pp.mark(str);
+	}
+
+}
